cfc53a307150091ad74479e847bbfa1f7802d802
Roger.Cui <jesuie@live.cn>
2016-07-15 02:05:34 +0800

    add demo

---
+++ sqldiff/sqldiff/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqldiff
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Please input sql file full path to compare diff with the previous version...");
+            }
+
+            if (args.Length == 2)
+            {
+                try
+                {
+                    var factory = new Factory(args[0]);
+                    factory.Run();
+                }
+                catch (Exception ex)
+                {
+                    throw new Exception(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ sqldiff/sqldiff/SystemEvent.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sqldiff
+{
+    [Serializable]
+    public class SystemEvent
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string SortKey { get; set; }
+        public string Description { get; set; }
+        public string Category1 { get; set; }
+        public string Category2 { get; set; }
+        public string CustomerSPERA { get; set; }
+        public string DeviceSPERA { get; set; }
+        public string ProductPERA { get; set; }
+        public string InvoiceSPERA { get; set; }
+        public string AccountSPERA { get; set; }
+        public string EnableRequireReason { get; set; }
+        public string EnableEditReason { get; set; }
+        public string AgreementSPERA { get; set; }
+        public string ShippingOrderSPERA { get; set; }
+        public string WorkOrderSPERA { get; set; }
+        public string EntityKey { get; set; }
+        public string IsObsolete { get; set; }
+        public string AllowForLedgerAccount { get; set; }
+        public string SkipOnHistoryScreen { get; set; }
+        public string ContactSPEAR { get; set; }
+        public string IsSettlement { get; set; }
+    }
+}
